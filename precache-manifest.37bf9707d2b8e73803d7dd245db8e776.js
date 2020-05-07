d78c30d1dae2d8e3490582c0b2f3fcc4b8223aee
CariJG <48261019+CariJG@users.noreply.github.com>
2020-05-06 22:04:08 -0500

    Updates

---
+++ precache-manifest.37bf9707d2b8e73803d7dd245db8e776.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d332743778ea43420616f2b1d16e952d",
+    "url": "/Prueba-ReactJS/index.html"
+  },
+  {
+    "revision": "edfac59b8a5a8f8cdb1c",
+    "url": "/Prueba-ReactJS/static/css/main.0bce0bba.chunk.css"
+  },
+  {
+    "revision": "1e6b7794e239027becc6",
+    "url": "/Prueba-ReactJS/static/js/2.d77b545a.chunk.js"
+  },
+  {
+    "revision": "ccb787817ba958d2ef7c458dc165f568",
+    "url": "/Prueba-ReactJS/static/js/2.d77b545a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "edfac59b8a5a8f8cdb1c",
+    "url": "/Prueba-ReactJS/static/js/main.23b21985.chunk.js"
+  },
+  {
+    "revision": "cb10dba3e61d14d86290",
+    "url": "/Prueba-ReactJS/static/js/runtime-main.6262ba4d.js"
+  }
+]);
